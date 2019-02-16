42ac4abdc0748f137b1d8ad9fb7e8f6b9d6fd6cb
jan <duldhardtjan@gmail.com>
2019-02-16 12:14:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/eventmanagement19/SelectEventActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventmanagement19
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_event)
+    }
+}
